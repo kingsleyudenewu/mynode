b516f1283d6a02b1495b68f0c82cabed40aaf14f
Udenewu Kingsley <kingzpacking@gmail.com>
2016-09-07 18:45:06 +0100

    New file added to perform unit testing with Chai on a node.js application

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,3 +1,10 @@
+//Mocha and CHai was used for our unit testing Mocha was install globally so i suggest when you're testing this application you do npm install -g mocha to install mocha globally while chai (npm install chai --save-dev) should be installed locally in a dev envinronment.
+var expect = require('chai').expect;
+var tools = require('../lib/tools');
 describe('printName()', function(){
-    it('should print the last name first');
+    it('should print the last name first', function(){
+        var results = tools.printName({first:'Kingsley', last:'Udenewu'});
+        expect(results).to.equal('Kingsley Udenewu');
+    });
+
 });
